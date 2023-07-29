f25eba6a3a232ef981c85d21f24567af505188bc
kaizervamph <kaizervamph@gmail.com>
2023-07-28 21:55:01 -0300

    Source

---
+++ Source/BackToEarth/BackToEarthGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BackToEarthGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BACKTOEARTH_API ABackToEarthGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
